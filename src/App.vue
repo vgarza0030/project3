71943bc936bea76493b2ff62a603be9602b357bd
Victor Garza <vgarza_0030@stu.southtexascollege.edu>
2025-02-05 17:42:35 -0600

    changed folders

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="countdown">
+    <input v-model.number="timeInput" type="number" min="1" max="60" placeholder="Enter time (1-60)" />
+    <button @click="startCountdown">Start Countdown</button>
+    <h2>{{ timeLeft }}</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const timeInput = ref(null);
+    const timeLeft = ref(null);
+    let countdown = null;
+
+    const startCountdown = () => {
+      if (!timeInput.value || timeInput.value < 1 || timeInput.value > 60) {
+        alert("Please enter a valid number between 1 and 60.");
+        return;
+      }
+      
+      timeLeft.value = timeInput.value;
+      clearInterval(countdown);
+      countdown = setInterval(() => {
+        if (timeLeft.value > 0) {
+          timeLeft.value--;
+        } else {
+          clearInterval(countdown);
+          alert("Time's up!");
+        }
+      }, 1000);
+    };
+
+    return { timeInput, timeLeft, startCountdown };
+  }
+};
+</script>
+
+<style>
+.countdown {
+  text-align: center;
+  font-family: Arial, sans-serif;
+  margin-top: 20px;
 }
 </style>
