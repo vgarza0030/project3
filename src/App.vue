210de9a0b74ec98404d9bd78745ad52dec352134
Victor Garza <vgarza_0030@stu.southtexascollege.edu>
2025-02-06 17:04:22 -0600

    updated the file

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,49 @@
-<script setup>
-import { ref } from 'vue';
-
-const timeInput = ref('');
-const time = ref(0);
-let countdown = null;
-
-const startCountdown = () => {
-    const parsedTime = parseInt(timeInput.value);
-    
-    if (isNaN(parsedTime) || parsedTime < 1 || parsedTime > 60) {
-        alert("Please enter a valid number between 1 and 60.");
-        return;
-    }
-
-    time.value = parsedTime;
-
-    if (countdown) {
-        clearInterval(countdown);
-    }
-
-    countdown = setInterval(() => {
-        time.value--;
-        if (time.value <= 0) {
-            clearInterval(countdown);
-            alert("Time's up! Have a great day!");
-        }
-    }, 1000);
-};
-</script>
-
 <template>
   <div class="container">
     <h1>Countdown Timer</h1>
     <h2>By Victor Garza</h2>
     <h3>Enter from 1-60</h3>
-    <input type="number" v-model="timeInput" placeholder="Enter seconds (1-60)" min="1" max="60" />
+    <input type="number" v-model.number="timeInput" placeholder="Enter seconds (1-60)" min="1" max="60">
     <button @click="startCountdown">Start Countdown</button>
-    <p>{{ time }}</p>
+    <p>{{ timeLeft }}</p>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      timeInput: null,
+      timeLeft: null,
+      countdown: null,
+    };
+  },
+  methods: {
+    startCountdown() {
+      if (isNaN(this.timeInput) || this.timeInput < 1 || this.timeInput > 60) {
+        alert("Please enter a valid number between 1 and 60.");
+        return;
+      }
+      
+      this.timeLeft = this.timeInput;
+      
+      if (this.countdown) clearInterval(this.countdown);
+      
+      this.countdown = setInterval(() => {
+        this.timeLeft--;
+        if (this.timeLeft <= 0) {
+          clearInterval(this.countdown);
+          alert("Time's up! Have a great day!");
+        }
+      }, 1000);
+    },
+  },
+};
+</script>
+
+
 <style scoped>
 .container {
-    text-align: center;
-    font-family: Arial, sans-serif;
-    margin-top: 50px;
-}
-input {
-    padding: 8px;
-    margin: 10px;
-}
-button {
-    padding: 10px;
-    background-color: blue;
-    color: white;
-    border: none;
-    cursor: pointer;
-}
-p {
-    font-size: 24px;
-    font-weight: bold;
+  text-align: center;
 }
 </style>
-
